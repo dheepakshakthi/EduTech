7dd0015b34d7bc1f9b8758fd87d9750ed2532e0c
unknown <jaswantraj150@gmail.com>
2025-12-17 12:04:32 +0530

    Endpoints Added

--- EduTech/urls.py
+++ EduTech/urls.py
@@ -2,10 +2,21 @@ from django.contrib import admin
 from django.urls import path
 from website import views
 
+from .views import (
+    signup_api,
+    login_api,
+    dashboard_stats_api,
+    recent_sessions_api,
+    recommendations_api
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
 
     path("api/signup/", views.signup_api, name="signup_api"),
     path("api/login/", views.login_api, name="login_api"),
+    path('api/dashboard/stats/', dashboard_stats_api),
+    path('api/sessions/recent/', recent_sessions_api),
+    path('api/recommendations/', recommendations_api),
 ]
--- website/views.py
+++ website/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password, check_password
+from django.http import JsonResponse
+from django.utils.timezone import now
 from .models import User
 
 def index(request):
@@ -60,3 +62,89 @@ def login_api(request):
             return JsonResponse({'success': False, 'message': str(e)})
     
     return JsonResponse({'success': False, 'message': 'Only POST allowed'})
+
+
+from django.db.models import Count
+
+from .models import User, Session
+def dashboard_stats_api(request):
+    if request.method == 'GET':
+        try:
+            total_students = User.objects.count()
+
+            today = now().date()
+            active_sessions_today = Session.objects.filter(
+                started_at__date=today
+            ).count()
+
+            stats = {
+                "total_students": total_students,
+                "active_sessions_today": active_sessions_today
+            }
+
+            return JsonResponse({
+                "success": True,
+                "data": stats
+            })
+
+        except Exception as e:
+            return JsonResponse({
+                "success": False,
+                "message": str(e)
+            })
+        
+
+def recent_sessions_api(request):
+    if request.method == 'GET':
+        try:
+            sessions_qs = Session.objects.select_related('user') \
+                .order_by('-started_at')[:3]
+
+            sessions = []
+            for s in sessions_qs:
+                sessions.append({
+                    "session_id": s.id,
+                    "user_name": s.user.name,
+                    "email": s.user.email,
+                    "title": s.title,
+                    "subject": s.subject,
+                    "started_at": s.started_at
+                })
+
+            return JsonResponse({
+                "success": True,
+                "data": sessions
+            })
+
+        except Exception as e:
+            return JsonResponse({
+                "success": False,
+                "message": str(e)
+            })
+        
+
+from .models import Recommendation
+def recommendations_api(request):
+    if request.method == 'GET':
+        try:
+            recommendations_qs = Recommendation.objects.all()[:3]
+
+            recommendations = []
+            for r in recommendations_qs:
+                recommendations.append({
+                    "id": r.id,
+                    "title": r.title,
+                    "icon": r.icon
+                })
+
+            return JsonResponse({
+                "success": True,
+                "data": recommendations
+            })
+
+        except Exception as e:
+            return JsonResponse({
+                "success": False,
+                "message": str(e)
+            })
+
